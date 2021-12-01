97dbba4fbb7655cf1711868a15e251014995d154
張永麒19660 <ESB19660@esunbank.com.tw>
2021-12-01 18:00:40 +0800

    Add Portfolio and Search page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/search">Search</router-link> |
+    <router-link to="/portfolio">Portfolio</router-link>
   </div>
   <router-view/>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,22 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Search.vue')
+  },
+  {
+    path: '/portfolio',
+    name: 'Portfolio',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Portfolio.vue')
   }
 ]
 
