a704bba1b3053b38df8bec5157b23848daac7237
張永麒19660 <ESB19660@esunbank.com.tw>
2021-12-02 11:33:42 +0800

    fix password eye slash and edit css

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,11 +2,12 @@
   <div class="search">
     <p>Room key: </p>
     <form method="post" class="search-block">
-        <input v-bind:type="passwordFieldType" name="password" autocomplete="current-password" required="" id="id_password">
-        <i v-bind:class="eyeClass" id="togglePassword" style="margin-left: -30px; cursor: pointer;" @click="switchVisibility"></i>
-        <button type="submit" class="btn btn-primary">Login</button>
+      <div class="search-room-key">
+        <input v-bind:type="passwordFieldType" name="password" required="" id="id_password">
+        <i class="far" v-bind:class="eyeClass" id="togglePassword" style="margin-left: -30px; cursor: pointer;" @click="switchVisibility"></i>
+      </div>
+      <button type="submit" class="btn btn-primary fas fa-search"></button>
     </form>
-
   </div>
 </template>
 
@@ -17,11 +18,11 @@ export default defineComponent({
 
     //var password = ref("");
     var passwordFieldType = ref("password");
-    var eyeClass = ref("far fa-eye");
+    var eyeClass = ref("fa-eye-slash");
     
     const switchVisibility = () => {
         passwordFieldType.value = passwordFieldType.value === "password" ? "text" : "password";
-        eyeClass.value = eyeClass.value === "far fa-eye" ? "far fa-eye-slash" : "far fa-eye";
+        eyeClass.value = eyeClass.value === "fa-eye-slash" ? "fa-eye" : "fa-eye-slash";
         console.log(passwordFieldType.value);
         console.log(eyeClass.value);
     };
@@ -32,23 +33,29 @@ export default defineComponent({
 
 <style scoped lang="scss">
 @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css";
-input{
+.search-block > .search-room-key > input{
     //border:none;
     border-top-width:0px;
     border-left-width:0px;
     border-right-width:0px;
     border-bottom-width:1px;
-
+    outline: none;
+    z-index: 100;
+}
+.search-block > .search-room-key{
+    margin-left:10px;
+    margin-right:25px;
 }
 .search-block{
     border:2px solid #000;
+    border-radius: 10px;
     display: inline-flex;
-    padding: 2px;
+    padding: 10px;
     align-items: center;
 }
 
 .search-block > button{
-    margin: 8px;
+    //margin: 8px;
 }
 
 </style>
