2681f0cf90fdb6f92089bbcaebfa62bfa80eaaf5
張永麒19660 <ESB19660@esunbank.com.tw>
2021-12-07 09:31:16 +0800

    Create component password toggle

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,15 +2,7 @@
   <div class="search">
 
     <DatePicker></DatePicker>
-    <p>Room key: </p>
-    <div class="search-block">
-      <div class="search-room-key">
-        <input v-bind:type="passwordFieldType" name="password" required="" id="id_password">
-        <i class="far" v-bind:class="eyeClass" id="togglePassword" style="margin-left: -30px; cursor: pointer;" @click="switchVisibility"></i>
-      </div>
-      <button type="submit" class="btn btn-primary fas fa-search" @click="getRoomInfo"></button>
-    </div>
-  
+    <passwordTextToggleComp></passwordTextToggleComp>
 
   </div>
 </template>
@@ -19,73 +11,20 @@
 import { defineComponent, ref, DefineComponent, inject } from "vue";
 import axios from "axios";
 import DatePicker from '@/components/DatePicker.vue';
+import passwordTextToggleComp from '@/components/passwordTextToggleComp.vue';
 export default defineComponent({
   components: {
-  DatePicker
+    DatePicker,
+    passwordTextToggleComp
   
   },
   setup() {
-
-    const dblocation = inject("dblocation");
-
-    //var password = ref("");
-    var passwordFieldType = ref("password");
-    var eyeClass = ref("fa-eye-slash");
-    
-    const switchVisibility = () => {
-        passwordFieldType.value = passwordFieldType.value === "password" ? "text" : "password";
-        eyeClass.value = eyeClass.value === "fa-eye-slash" ? "fa-eye" : "fa-eye-slash";
-        console.log(passwordFieldType.value);
-        console.log(eyeClass.value);
-    };
-
-    const getRoomInfo = () => {
-        axios
-          .post(dblocation + "customer/queryWithCustomerId", {
-            //requestBody: { actionInfo: null, requestItem: searchContent },
-          })
-          .then((response) => {
-            console.log("yes");
-          //   CustomerName.value =response.data.resultBody.responseResult.customerName;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-    };
-
-    return { passwordFieldType, switchVisibility, eyeClass, getRoomInfo };
+    return { };
   },
 });
 </script>
 
 <style scoped lang="scss">
-@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css";
-.search-block > .search-room-key > input{
-    //border:none;
-    border-top-width:0px;
-    border-left-width:0px;
-    border-right-width:0px;
-    border-bottom-width:1px;
-    outline: none;
-    z-index: 100;
-}
-.search-block > .search-room-key{
-    margin-left:10px;
-    margin-right:25px;
-}
-.search-block > .search-room-key > .far{
-    width:20px;
-}
-.search-block{
-    border:2px solid #000;
-    border-radius: 10px;
-    display: inline-flex;
-    padding: 10px;
-    align-items: center;
-}
 
-.search-block > button{
-    //margin: 8px;
-}
 
 </style>
